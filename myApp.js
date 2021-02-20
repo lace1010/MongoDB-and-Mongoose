9bd0aeb4ae387f9884bf0c5d0af30ee7bc8c018d
Hunter Lacefield <lace1010@gmail.com>
2021-02-20 21:40:32 +0800

    find object's key favoriteFoods value. not just the string

--- myApp.js
+++ myApp.js
@@ -142,7 +142,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find(foodToSearch)
+  Person.find({ favoriteFoods: foodToSearch })
     .sort({ name: "ascending" }) // Sorting all results found by name
     .limit(2) // How many results are allowed to pass
     .select({ age: -1 }) // Removes age from object (deselects it from model)
